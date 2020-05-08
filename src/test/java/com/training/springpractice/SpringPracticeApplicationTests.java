5102d8322eb1a75e8661304c17884aa2da8ae5c0
Kevin Alvarez <kevin.alvarez.ortiz.15@gmail.com>
2020-05-08 12:59:52 -0400

    ftr: module renamed

--- src/test/java/com/training/springpractice/SpringPracticeApplicationTests.java
+++ src/test/java/com/training/springpractice/SpringPracticeApplicationTests.java
@@ -1,10 +1,10 @@
-package com.training.movies;
+package com.training.springpractice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MoviesApplicationTests {
+class SpringPracticeApplicationTests {
 
 	@Test
 	void contextLoads() {

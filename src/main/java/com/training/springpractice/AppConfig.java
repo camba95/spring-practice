363fc5bec1e40151919eccd39d61e375eae9f312
Kevin Alvarez <kevin.alvarez.ortiz.15@gmail.com>
2020-05-10 18:50:48 -0400

    ftr: bean config changed

--- src/main/java/com/training/springpractice/AppConfig.java
+++ src/main/java/com/training/springpractice/AppConfig.java
@@ -1,15 +1,9 @@
 package com.training.springpractice;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan({"com.training.springpractice"})
 public class AppConfig {
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
 }

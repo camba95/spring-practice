bc20e740f5879dc6041d3ebb21810fd98189d2d4
Kevin Alvarez <kevin.alvarez.ortiz.15@gmail.com>
2020-05-13 01:05:59 -0400

    ftr: validate copies

--- src/main/java/com/training/springpractice/repositories/CatalogRepository.java
+++ src/main/java/com/training/springpractice/repositories/CatalogRepository.java
@@ -1,7 +1,16 @@
 package com.training.springpractice.repositories;
 
 import com.training.springpractice.models.Catalog;
+import com.training.springpractice.models.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CatalogRepository extends JpaRepository<Catalog, Long> {
+    @Modifying
+    @Query("update movie_catalog mc set mc.copies = mc.copies - 1 where mc.movie.id = :movieId")
+    void decreaseCopies(@Param("movieId") Long movieId);
+
+    Catalog findByMovie(Movie movie);
 }
--- src/main/java/com/training/springpractice/services/RentalService.java
+++ src/main/java/com/training/springpractice/services/RentalService.java
@@ -1,10 +1,9 @@
 package com.training.springpractice.services;
 
+import com.training.springpractice.errors.BadRequestException;
 import com.training.springpractice.errors.NotFoundException;
-import com.training.springpractice.models.Member;
-import com.training.springpractice.models.Movie;
-import com.training.springpractice.models.Rental;
-import com.training.springpractice.models.RentalBody;
+import com.training.springpractice.models.*;
+import com.training.springpractice.repositories.CatalogRepository;
 import com.training.springpractice.repositories.MemberRepository;
 import com.training.springpractice.repositories.MovieRepository;
 import com.training.springpractice.repositories.RentalRepository;
@@ -12,6 +11,7 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -27,16 +27,22 @@ public class RentalService {
     @Autowired
     private MemberRepository memberRepository;
 
+    @Autowired
+    private CatalogRepository catalogRepository;
+
     private ModelMapper mapper;
 
     public RentalService() {}
 
+    @Transactional
     public RentalBody create (RentalBody body) throws RuntimeException {
         Optional<Movie> movie = movieRepository.findById(body.getMovie_id());
         Optional<Member> member = memberRepository.findById(body.getMember_id());
         if (movie.isPresent() && member.isPresent()) {
+            validateCopies(movie.get());
             Rental rental = mapper.map(body, Rental.class);
             Rental createdRental = repository.saveAndFlush(rental);
+            catalogRepository.decreaseCopies(movie.get().getId());
             return mapper.map(createdRental, RentalBody.class);
         }
         throw new NotFoundException("Invalid information");
@@ -59,6 +65,13 @@ public class RentalService {
                 map().setMember_id(source.getMember().getId());
             }
         });
+    }
+
+    private void validateCopies(Movie movie) {
+        Catalog catalog = catalogRepository.findByMovie(movie);
+        if (catalog.getCopies() <= 0) {
+            throw new BadRequestException("Movie not available");
+        }
     }
 
     private Rental convertToEntity(RentalBody body) {
